0c907bddf3b7002e5f51c28a0ae3e731dc8b6c17
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-03-13 00:24:18 -0500

    add test

---
+++ tests/cvmfs_grid_test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#this is a test that actually runs the dilepton workflow to test out grid access and cvmfs access
+
+mkdir /workdir/inputs
+cd /workdir/inputs
+curl http://physics.nyu.edu/~lh1132/recast_zara/255101.zip -O
+unzip 255101.zip
+yadage-run -t from-github /workdir ewk_analyses/ewkdilepton_analysis/example_workflow.yml /workdir/inputs/input.yaml
