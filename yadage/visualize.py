711ee02954cd99aa5663ada53f1cf53fa7373728
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-03-19 02:53:55 -0400

    style fix

--- yadage/visualize.py
+++ yadage/visualize.py
@@ -20,6 +20,52 @@ def write_stage_graph(workdir,workflow):
     write_dot(graph,'{}/adage_stages.dot'.format(workdir))
     subprocess.call(shlex.split('dot -Tpdf {}/yadage_stages.dot'.format(workdir)),
                     stdout = open('{}/yadage_stages.pdf'.format(workdir),'w'))
+
+
+def add_step_to_cluster(step,adagegraph,cluster,fullgraph):
+    stepid = step.identifier
+
+    pars = step.task.attributes.copy()
+    # print pars
+    parstrings =  [': '.join((k,str(pars[k]))) for k in sorted(pars.keys())]
+
+
+    step_report = u'''\
+{name}
+______
+{pars}
+'''
+
+    rep = step_report.format(name = step.name,
+                             pars = '\n'.join(parstrings))
+
+    attr = {'color':'red','label':rep,'shape':'box'}    
+    cluster.add_node(pydotplus.graphviz.Node(name = stepid, obj_dict = None, **attr))
+
+    #add outputs circles
+    for k,v in step.result_of().iteritems():
+        for i,y in enumerate(v if type(v)==list else [v]):
+            name = 'output_{}_{}_{}'.format(step.task.name,k,i)
+        
+            attr = {'label':'{}_{}: {}'.format(k,i,y),'color':'blue'}
+            cluster.add_node(pydotplus.graphviz.Node(name, obj_dict = None, **attr))
+            cluster.add_edge(pydotplus.graphviz.Edge(stepid,name))
+
+    #connect node to outputs
+    #if input information is there, add edge to input
+    if step.task.inputs:
+        for k,inputs_from_node in step.task.inputs.iteritems():
+            for one in inputs_from_node:
+                depname = 'output_{}_{}_{}'.format(k,one[0],one[1])
+                fullgraph.add_edge(pydotplus.graphviz.Edge(depname,stepid))
+
+
+    #if not, we'll just add to the dependent node directly
+    else:
+        for pre in adagegraph.predecessors(stepid):
+            log.warning('really no inputs to this node but predecessors?: %s',step)
+            fullgraph.add_edge(pydotplus.graphviz.Edge(pre,stepid))
+
     
 def write_prov_graph(workdir,adagegraph,workflow):
     provgraph = pydotplus.graphviz.Graph()
@@ -31,51 +77,7 @@ def write_prov_graph(workdir,adagegraph,workflow):
 
             stagecluster = pydotplus.graphviz.Cluster(graph_name = stage, label = stage)
             provgraph.add_subgraph(stagecluster)
-
-            pars = step.task.attributes.copy()
-    
-            # print pars
-            parstrings =  [': '.join((k,str(pars[k]))) for k in sorted(pars.keys())]
-            
-            step_report = u'''\
-{name}
-______
-{pars}
-'''
-    
-            rep = step_report.format(name = step.name,
-                                     pars = '\n'.join(parstrings))
-    
-            attr = {'color':'red','label':rep,'shape':'box'}    
-            stagecluster.add_node(pydotplus.graphviz.Node(name = x, obj_dict = None, **attr))
-    
-            #add outputs circles
-            for k,v in step.result_of().iteritems():
-                for i,y in enumerate(v if type(v)==list else [v]):
-                    name = 'output_{}_{}_{}'.format(step.task.name,k,i)
-                    
-    
-                    attr = {'label':'{}_{}: {}'.format(k,i,y),'color':'blue'}
-                    stagecluster.add_node(pydotplus.graphviz.Node(name, obj_dict = None, **attr))
-    
-                    provgraph.add_edge(pydotplus.graphviz.Edge(x,name))
-    
-            nodeinfo =  step.task
-            
-            #connect node to outputs
-            #if input information is there, add edge to input
-            if nodeinfo.inputs:
-                for k,inputs_from_node in nodeinfo.inputs.iteritems():
-                    for one in inputs_from_node:
-                        depname = 'output_{}_{}_{}'.format(k,one[0],one[1])
-                        provgraph.add_edge(pydotplus.graphviz.Edge(depname,x))
-    
-    
-            #if not, we'll just add to the dependent node directly
-            else:
-                for pre in adagegraph.predecessors(x):
-                    log.warning('really no inputs to this node but predecessors?: %s',x)
-                    provgraph.add_edge(pydotplus.graphviz.Edge(pre,x))
+            add_step_to_cluster(step,adagegraph,stagecluster,provgraph)
             
                 
         
