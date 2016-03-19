7b29d01a0a52fa34ab699d0f7d883f6daaf0075b
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-03-19 03:03:30 -0400

    style fix

--- yadage/visualize.py
+++ yadage/visualize.py
@@ -4,7 +4,6 @@ import networkx as nx
 from networkx.drawing.nx_pydot import write_dot
 import logging
 import subprocess
-
 log = logging.getLogger(__name__)
 
 def simple_stage_graph(workflow):
@@ -21,14 +20,12 @@ def write_stage_graph(workdir,workflow):
     subprocess.call(shlex.split('dot -Tpdf {}/yadage_stages.dot'.format(workdir)),
                     stdout = open('{}/yadage_stages.pdf'.format(workdir),'w'))
 
-
 def add_step_to_cluster(step,adagegraph,cluster,fullgraph):
     stepid = step.identifier
 
     pars = step.task.attributes.copy()
     # print pars
     parstrings =  [': '.join((k,str(pars[k]))) for k in sorted(pars.keys())]
-
 
     step_report = u'''\
 {name}
@@ -39,48 +36,44 @@ ______
     rep = step_report.format(name = step.name,
                              pars = '\n'.join(parstrings))
 
-    attr = {'color':'red','label':rep,'shape':'box'}    
-    cluster.add_node(pydotplus.graphviz.Node(name = stepid, obj_dict = None, **attr))
+    cluster.add_node(pydotplus.graphviz.Node(
+                name = stepid,
+                obj_dict = None,
+                color = 'red',
+                label = rep,
+                shape = 'box')
+    )
 
     #add outputs circles
     for k,v in step.result_of().iteritems():
         for i,y in enumerate(v if type(v)==list else [v]):
             name = 'output_{}_{}_{}'.format(step.task.name,k,i)
-        
-            attr = {'label':'{}_{}: {}'.format(k,i,y),'color':'blue'}
-            cluster.add_node(pydotplus.graphviz.Node(name, obj_dict = None, **attr))
+            cluster.add_node(pydotplus.graphviz.Node(name, label = '{}_{}: {}'.format(k,i,y), color = 'blue'))
             cluster.add_edge(pydotplus.graphviz.Edge(stepid,name))
 
     #connect node to outputs
-    #if input information is there, add edge to input
     if step.task.inputs:
+        #if input information is there, add edge to input
         for k,inputs_from_node in step.task.inputs.iteritems():
             for one in inputs_from_node:
                 depname = 'output_{}_{}_{}'.format(k,one[0],one[1])
                 fullgraph.add_edge(pydotplus.graphviz.Edge(depname,stepid))
-
-
-    #if not, we'll just add to the dependent node directly
     else:
+        #if not, we'll just add to the dependent node directly
         for pre in adagegraph.predecessors(stepid):
             log.warning('really no inputs to this node but predecessors?: %s',step)
             fullgraph.add_edge(pydotplus.graphviz.Edge(pre,stepid))
 
-    
 def write_prov_graph(workdir,adagegraph,workflow):
     provgraph = pydotplus.graphviz.Graph()
     
     stagedict = {stage['name']:stage for stage in workflow['stages']}
     for stage in nx.topological_sort(simple_stage_graph(workflow)):
-
+        stagecluster = pydotplus.graphviz.Cluster(graph_name = stage, label = stage)
+        provgraph.add_subgraph(stagecluster)
         for step in stagedict[stage]['scheduled_steps']:
-
-            stagecluster = pydotplus.graphviz.Cluster(graph_name = stage, label = stage)
-            provgraph.add_subgraph(stagecluster)
             add_step_to_cluster(step,adagegraph,stagecluster,provgraph)
             
-                
-        
     with open('{}/yadage_workflow_instance.dot'.format(workdir),'w') as dotfile:
         dotfile.write(provgraph.to_string())
 
