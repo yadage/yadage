a184ab63488f9eacf8412127ae20676ac57141b5
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-03-19 04:38:56 -0400

    remove print statements

--- yadage/handlers/utils.py
+++ yadage/handlers/utils.py
@@ -5,3 +5,18 @@ def handler_decorator():
             handlers[name] = func
         return wrap
     return handlers,decorator
+    
+def evaluate_parameters(parameters,context):
+    """
+    values of in context are converted to strings via json.dump,
+    parameters then interpolated, and finally reloaded as json
+    """
+    dumped_context = {k:json.dumps(v) for k,v in context.iteritems()}
+    evaluated = {}
+    for k,v in parameters.iteritems():
+        eval_val = v.format(**dumped_context)
+        try:
+            evaluated[k] = json.loads(eval_val)
+        except ValueError:
+            evaluated[k] = eval_val
+    return evaluated
