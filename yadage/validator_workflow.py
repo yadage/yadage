901c7502dd13925dfcd72207d447c74153fc21a8
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-05-04 18:40:46 -0400

    validator

--- yadage/validator_workflow.py
+++ yadage/validator_workflow.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 import os
-import capschemas
 import click
 import jsonschema
-import workflow_loader
 import logging
+import workflow_loader
+import capschemas
+import json
 
-logging.basicConfig(level = logging.INFO)
+logging.basicConfig(level = logging.ERROR)
 log = logging.getLogger(__name__)
 @click.command()
 @click.argument('workflow')
-@click.argument('toplevel', default = None)
-@click.argument('schemadir', default = capschemas.schemadir)
+@click.argument('toplevel', default = '')
+@click.argument('schemadir', default = '')
 def main(workflow,toplevel,schemadir):
     if not toplevel:
         toplevel = os.getcwd()
+    if not schemadir:
+        schemadir = capschemas.schemadir
     try:
         data = workflow_loader.workflow(workflow, toplevel = toplevel, schemadir = schemadir, validate = True)
         click.secho('workflow validates against schema', fg = 'green')
@@ -22,6 +25,7 @@ def main(workflow,toplevel,schemadir):
         log.exception('validation error')
         click.secho('workflow does not validate against schema', fg = 'red')
     except:
+        # log.exception('')
         click.secho('this is not even wrong (non-ValidationError exception)', fg = 'red')
 
         
