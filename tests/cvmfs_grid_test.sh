81adecb8de3be17d2e4a4465e7906723a5ab9024
Lukas <lukas.heinrich@gmail.com>
2016-05-05 14:57:10 -0400

    Update cvmfs_grid_test.sh

--- tests/cvmfs_grid_test.sh
+++ tests/cvmfs_grid_test.sh
@@ -6,4 +6,4 @@ mkdir /workdir/inputs
 cd /workdir/inputs
 curl http://physics.nyu.edu/~lh1132/recast_zara/255123.zip -O
 unzip 255123.zip
-yadage-run -t from-github /workdir ewk_analyses/ewkdilepton_analysis/example_workflow.yml /workdir/inputs/input.yaml
+yadage-run -t from-github /workdir ewk_analyses/ewkdilepton_analysis/ewk_dilepton_recast_workflow.yml /workdir/inputs/input.yaml
