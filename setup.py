467724652e2e4b521f1794b589ec5bc4b48d22f9
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-05-05 15:18:46 -0400

    bump adage version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
       ],
   },
   dependency_links = [
-      'https://github.com/lukasheinrich/adage/tarball/master#egg=adage-0.3.0',
+      'https://github.com/lukasheinrich/adage/tarball/master#egg=adage-0.3.1',
       'https://github.com/lukasheinrich/packtivity/tarball/master#egg=packtivity-0.0.2',
       'https://github.com/lukasheinrich/cap-schemas/tarball/master#egg=cap-schemas-0.0.1'
   ]
