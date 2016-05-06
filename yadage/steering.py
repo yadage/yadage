2f7aa94a5df00bcc9be17815d582b82632b66734
Lukas Heinrich <lukas.heinrich@gmail.com>
2016-05-06 12:11:52 -0400

    run!

--- yadage/steering.py
+++ yadage/steering.py
@@ -14,12 +14,13 @@ log = logging.getLogger(__name__)
 @click.option('-v','--verbosity', default = 'INFO')
 @click.option('-i','--loginterval', default = 30)
 @click.option('-c','--schemasource', default = capschemas.schemadir)
+@click.option('-p','--parallel', default = 2)
 @click.argument('analysis')
 @click.argument('initdata', default = '')
-def main(workdir,analysis,initdata,toplevel,verbosity,loginterval,schemasource):
+def main(workdir,analysis,initdata,toplevel,verbosity,loginterval,schemasource,parallel):
     logging.basicConfig(level = getattr(logging,verbosity))
     initdata = yaml.load(open(initdata)) if initdata else {}
-    steering_api.run_workflow(workdir,analysis,initdata,toplevel,loginterval,schemadir = schemasource)
+    steering_api.run_workflow(workdir,analysis,initdata,toplevel,loginterval,schemadir = schemasource, nparallel = parallel)
 
 if __name__ == '__main__':
     main()
