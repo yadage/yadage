42b734fe33c9067b80062671d93893cd818c2a96
lukasheinrich <lukas.heinrich@gmail.com>
2016-07-13 10:56:12 +0200

    db from env

--- yadage/backends/celeryapp.py
+++ yadage/backends/celeryapp.py
@@ -1,2 +1,8 @@
 from celery import Celery
+import os
+redis = 'redis://{hostname}:{port}/{database}'.format(
+  hostname = os.environ.get('YADAGE_REDIS_HOST','localhost'),
+  port = os.environ.get('YADAGE_REDIS_PORT',6379),
+  database = os.environ.get('YADAGE_REDIS_DB',0)
+)
 app = Celery(broker = 'redis://', backend = 'redis://')
