23d98c98deb893f430b9d1531c7231dbba568a40
lukasheinrich <lukas.heinrich@gmail.com>
2016-08-04 17:02:30 +0200

    Bump version: 0.5.0 → 0.5.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'yadage',
-  version = '0.5.0',
+  version = '0.5.1',
   description = 'yadage - YAML based adage',
   url = '',
   author = 'Lukas Heinrich',
@@ -11,7 +11,7 @@ setup(
   include_package_data = True,
   install_requires = [
     'functools32',
-    'adage>0.5.0',
+    'adage>0.5.1',
     'packtivity>0.0.1',
     'cap-schemas',
     'click',
