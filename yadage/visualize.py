7244ac1233cdf4d4b6b957a2cc878ba309384f3c
lukasheinrich <lukas.heinrich@gmail.com>
2016-08-16 20:27:36 +0200

    simple viz

--- yadage/visualize.py
+++ yadage/visualize.py
@@ -1,27 +1,11 @@
 import shlex
 import pydotplus
 import jsonpointer
-import networkx as nx
-from networkx.drawing.nx_pydot import write_dot
 import logging
 import jq
 import subprocess
 
 log = logging.getLogger(__name__)
-
-def simple_stage_graph(workflow):
-    graph = nx.DiGraph()
-    for stage in workflow.stages.values():
-        graph.add_node(stage.name)
-        for x in stage.dependencies:
-            graph.add_edge(x,stage.name)
-    return graph
-
-def write_stage_graph(workdir,workflow):
-    graph = simple_stage_graph(workflow)
-    write_dot(graph,'{}/yadage_stages.dot'.format(workdir))
-    subprocess.call(shlex.split('dot -Tpdf {}/yadage_stages.dot'.format(workdir)),
-                    stdout = open('{}/yadage_stages.pdf'.format(workdir),'w'))
 
 def output_id(stepid,outputkey,index):
     identifier = 'output_{}_{}'.format(stepid,outputkey)
@@ -38,44 +22,6 @@ def add_outputs_to_cluster(step,cluster):
             label += ' {}'.format(y)
             cluster.add_node(pydotplus.graphviz.Node(name, label = label, color = 'blue'))
             cluster.add_edge(pydotplus.graphviz.Edge(step.identifier,name))
-
-def add_step_to_cluster(step,workflow,stagecluster,provgraph):
-    step = workflow.dag.getNode(step)
-    stepid = step.identifier
-
-    pars = step.task.attributes.copy()
-    parstrings =  [': '.join((k,str(pars[k]))) for k in sorted(pars.keys())]
-
-    step_report = u'''\
-{name}
-______
-{pars}
-'''
-
-    rep = step_report.format(name = step.name, pars = '\n'.join(parstrings))
-    stagecluster.add_node(pydotplus.graphviz.Node(
-                name = stepid,
-                obj_dict = None,
-                color = 'red',
-                label = rep,
-                shape = 'box'
-        )
-    )
-    add_outputs_to_cluster(step,stagecluster)
-
-    #connect node to outputs
-    if step.task.inputs:
-        # if input information is there, add edge to input
-        for reference in step.task.inputs:
-            refid = reference.stepid
-            refptr = reference.pointer
-            index =  refptr.parts[1] if len(refptr.parts)>1 else None
-            provgraph.add_edge(pydotplus.graphviz.Edge(output_id(refid,refptr.parts[0],index),stepid))
-    else:
-        #if not, we'll just add to the dependent node directly
-        for pre in workflow.dag.predecessors(stepid):
-            log.warning('really no inputs to this node but predecessors?: %s',step)
-            provgraph.add_edge(pydotplus.graphviz.Edge(pre,stepid))
 
 def fillscope(cluster,workflow,scope = ''):
     # scopecluster = stagecluster = pydotplus.graphviz.Cluster(graph_name = '_'.join(stagescopeprts),
