1dce63d7b418d92692c5887b4d1c4703721a692a
lukasheinrich <lukas.heinrich@gmail.com>
2016-09-27 00:56:40 +0200

    eval type yadge without pull

--- yadagelocaldocker.sh
+++ yadagelocaldocker.sh
@@ -1,4 +1,4 @@
 function yadage-run {
-   docker run --rm -it -e PACKTIVITY_DOCKER_NOPULL=true -v $PWD:$PWD -w $PWD -v /var/run/docker.sock:/var/run/docker.sock lukasheinrich/yadage yadage-run $*
+   docker run --rm -it -e PACKTIVITY_WITHIN_DOCKER=true -e PACKTIVITY_WITHIN_DOCKER=true -v $PWD:$PWD -w $PWD -v /var/run/docker.sock:/var/run/docker.sock lukasheinrich/yadage yadage-run $*
 }
 
