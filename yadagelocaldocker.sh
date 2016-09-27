862bcb9c0ea77124fbfa4e6bfbc7bde2f17936ac
Lukas <lukas.heinrich@gmail.com>
2016-09-27 15:01:00 +0200

    Update yadagelocaldocker.sh

--- yadagelocaldocker.sh
+++ yadagelocaldocker.sh
@@ -1,4 +1,4 @@
 function yadage-run {
-   docker run --rm -it -e PACKTIVITY_DOCKER_NO_PULL=true -e PACKTIVITY_WITHIN_DOCKER=true -v $PWD:$PWD -w $PWD -v /var/run/docker.sock:/var/run/docker.sock lukasheinrich/yadage yadage-run $*
+   docker run --rm -it -e PACKTIVITY_DOCKER_NOPULL=true -e PACKTIVITY_WITHIN_DOCKER=true -v $PWD:$PWD -w $PWD -v /var/run/docker.sock:/var/run/docker.sock lukasheinrich/yadage yadage-run $*
 }
 
