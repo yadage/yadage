062aae8fde30017e96369f5c98101694e565fcb9
lukasheinrich <lukas.heinrich@gmail.com>
2016-10-31 22:06:49 +0100

    Bump version: 0.7.3 → 0.8.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'yadage',
-  version = '0.7.3',
+  version = '0.8.0',
   description = 'yadage - YAML based adage',
   url = '',
   author = 'Lukas Heinrich',
