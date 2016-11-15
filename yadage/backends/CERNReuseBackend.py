596b1f1200d12bcfc95b0118abed8b9dcd9dba85
lukasheinrich <lukas.heinrich@gmail.com>
2016-11-15 16:36:12 +0100

    CERN ReuseBranch

---
+++ yadage/backends/CERNReuseBackend.py
@@ -0,0 +1,41 @@
+from packtivitybackend import PacktivityProxyBase
+
+class CERNReuseBackend(PacktivityProxyBase):
+    '''
+    A proxy to a job submitted to the CERN (still unnamed) reuse backend
+    '''
+    def __init__(self):
+        pass
+
+    def details(self):
+        return None
+        # return {
+        #     'task_id': self.proxy.task_id,
+        # }
+
+    @classmethod
+    def fromJSON(cls, data):
+        # proxy = AsyncResult(
+        #     data['proxydetails']['task_id'],
+        #     task_name=data['proxydetails']['task_name']
+        # )
+        return cls(proxy)
+
+class CERNReuseBackend(object):
+    '''
+    The CERN (still unnamed) reuse backend
+    '''
+    def submit(self, task):
+        raise NotImplementedError('wait for it...')
+
+    def result(self, resultproxy):
+        raise NotImplementedError('wait for it...')
+
+    def ready(self, resultproxy):
+        raise NotImplementedError('wait for it...')
+
+    def successful(self, resultproxy):
+        raise NotImplementedError('wait for it...')
+
+    def fail_info(self, resultproxy):
+        raise NotImplementedError('wait for it...')
