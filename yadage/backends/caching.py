1d3f8cf41e6d0c910d89d2a5a2d7e1c4e9f5009e
lukasheinrich <lukas.heinrich@gmail.com>
2016-11-15 02:34:08 +0100

    ok

---
+++ yadage/backends/caching.py
@@ -0,0 +1,49 @@
+import federatedbackend
+import json
+from yadage.helpers import get_obj_id
+from packtivitybackend import PacktivityTrivialProxy
+
+import logging
+log = logging.getLogger(__name__)
+
+class CachedBackend(federatedbackend.FederatedBackend):
+    def __init__(self,backend,cacheconfig = None):
+        super(CachedBackend,self).__init__({
+            'cache':TrivialBackend(),
+            'primary':backend
+        })
+        cachefile = cacheconfig
+        self.cache = json.load(open(cachefile)) if cachefile else None
+
+    def cachevalid(self,task):
+        return True #ToDo :)
+
+    def cacheddata(self,task):
+        hashid = get_obj_id(task)
+        if not self.cache: return None
+        if not self.cachevalid(task): return None
+        log.info('use cached result for task: {}'.format(task))
+        return self.cache.get(hashid,None)
+
+    def routedsubmit(self,task):
+        cached = self.cacheddata(task)
+        if cached:
+            return PacktivityTrivialProxy(**cached)
+        else:
+            return self.backends['primary'].submit(task)
+
+    def routeproxy(self,proxy):
+        if type(proxy) == PacktivityTrivialProxy:
+            return 'cache'
+        else:
+            return 'primary'
+
+class TrivialBackend(object):
+    def result(self,resultproxy):
+        return resultproxy.result
+
+    def ready(self,resultproxy):
+        return True
+
+    def successful(self,resultproxy):
+        return resultproxy.status == 'SUCCESS'
---
+++ yadage/backends/federatedbackend.py
@@ -0,0 +1,32 @@
+class FederatedBackend(object):
+    '''
+    A meta backend that routes tasks to several internal backends
+    '''
+    def __init__(self,backends):
+        '''takes a dictionary of backendname->backendobject and a router'''
+        self.backends = backends
+
+    def routedsubmit(self,task):
+        raise NotImplementedError('needs implementation')
+
+    def routeproxy(self,proxy):
+        raise NotImplementedError('needs implementation')
+
+    def submit(self,task):
+        return self.routedsubmit(task)
+
+    def result(self,proxy):
+        b = self.routeproxy(proxy)
+        return self.backends[b].result(proxy)
+
+    def ready(self,proxy):
+        b = self.routeproxy(proxy)
+        return self.backends[b].ready(proxy)
+
+    def successful(self,proxy):
+        b = self.routeproxy(proxy)
+        return self.backends[b].successful(proxy)
+
+    def fail_info(self,proxy):
+        b = self.routeproxy(proxy)
+        return self.backends[b].fail_info(proxy)
