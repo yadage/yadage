c768798a35b5cba217f041fc686c3ea4bca394be
lukasheinrich <lukas.heinrich@gmail.com>
2016-11-15 14:53:59 +0100

    actually add trivial backend

---
+++ yadage/backends/trivialbackend.py
@@ -0,0 +1,30 @@
+class TrivialProxy(object):
+    '''
+    A trivial proxy that carries the results and status already
+    '''
+    def __init__(self,status, result):
+        self.status = status
+        self.result = result
+
+    def json(self):
+        return None
+
+class TrivialBackend(object):
+    '''
+    A trivial backend that can only return proxy based information, no submission
+    '''
+    def fail_info(self,resultproxy):
+        raise NotImplementedError('The trivial proxy is not made for submission')
+
+    def result(self,resultproxy):
+        return resultproxy.result
+
+    def ready(self,resultproxy):
+        #when we have a proxy it is by definition ready...
+        return True
+
+    def successful(self,resultproxy):
+        return resultproxy.status == 'SUCCESS'
+
+    def fail_info(self,resultproxy):
+        return None
