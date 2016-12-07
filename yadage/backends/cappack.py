388ee913a4b35100a13948eafd86296ac82b0866
lukasheinrich <lukas.heinrich@gmail.com>
2016-12-07 16:23:58 +0100

    import from env vars

--- yadage/backends/cappack.py
+++
@@ -1,7 +0,0 @@
-from packtivity.handlers.environment_handlers import environment
-import logging
-log = logging.getLogger('yadage.cap')
-
-@environment('docker-encapsulated','cap')
-def run_in_env(environment,context,job):
-    log.info('running an job for CAP!!')
