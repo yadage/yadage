78748224498ce7b1d4ac5768d2a959b21fdd3bd3
lukasheinrich <lukas.heinrich@gmail.com>
2017-03-17 02:53:45 +0100

    RTD tweak

--- docs/conf.py
+++ docs/conf.py
@@ -338,3 +338,14 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #
 # texinfo_no_detailmenu = False
+
+
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['packtivity.statecontexts.posixfs_context']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
