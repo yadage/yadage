bcc10319017514d39136e4dc6014c7d693e3d15b
lukasheinrich <lukas.heinrich@gmail.com>
2017-06-12 02:39:47 +0200

    manualcli

--- tests/test_manualcli.py
+++ tests/test_manualcli.py
@@ -8,7 +8,14 @@ def test_manual(tmpdir):
     workdir   = os.path.join(str(tmpdir),'workdir')
     statefile = os.path.join(str(tmpdir),'state.json')
     result = runner.invoke(yadage.manualcli.init,[workdir,'workflow.yml','-t','testspecs/local-helloworld','-s','filebacked:'+statefile])
+    assert result.exit_code == 0
+
+    result = runner.invoke(yadage.manualcli.preview,['-s','filebacked:'+statefile,'/hello_world'])
+    assert result.exit_code == 0
+
     result = runner.invoke(yadage.manualcli.apply,['-s','filebacked:'+statefile,'-n','/hello_world'])
+    assert result.exit_code == 0
+
     result = runner.invoke(yadage.manualcli.show,['-s','filebacked:'+statefile])
     assert result.exit_code == 0
 
