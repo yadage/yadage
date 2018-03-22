e4b84d05c31465eb6d3ffc683dd3e543871364f7
lukasheinrich <lukas.heinrich@gmail.com>
2018-03-22 03:21:44 +0100

    Bump version: 0.14.1 → 0.14.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if 'READTHEDOCS' in os.environ:
 
 setup(
   name = 'yadage',
-  version = '0.14.1',
+  version = '0.14.2',
   description = 'yadage - YAML based adage',
   url = '',
   author = 'Lukas Heinrich',
