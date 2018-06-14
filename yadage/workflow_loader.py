f9480bf05d0798f4f7225c55ac55e77aa8087e5b
lukasheinrich <lukas.heinrich@gmail.com>
2018-06-14 02:38:21 +0200

    dialect changes in upstream

--- yadage/workflow_loader.py
+++ yadage/workflow_loader.py
@@ -1,5 +1,6 @@
 import yadageschemas
 
+
 def workflow(source, toplevel, schema_name='yadage/workflow-schema', schemadir=None, validate=True):
     '''
     load (and validate) workflow from source
@@ -7,9 +8,28 @@ def workflow(source, toplevel, schema_name='yadage/workflow-schema', schemadir=N
     param toplevel: base URI to resolve references from
     schemadir
     '''
-    data = yadageschemas.load(source, toplevel, schema_name, schemadir, validate)
+    dialect, spec = ('raw_with_defaults', source) if ':' not in source else source.split(':')
+
+    specopts = {
+        'toplevel': toplevel,
+        'schema_name': schema_name,
+        'schemadir': schemadir,
+        'load_as_ref': False
+    }
+    validopts = {
+        'schema_name': schema_name,
+        'schemadir': schemadir,
+    }
+
+    data = yadageschemas.load(
+        spec, specopts, validate = True, validopts = validopts, dialect = dialect
+    )
     return data
 
 def validate(data, schema_name='yadage/workflow-schema', schemadir=None):
     '''validate workflow data'''
-    yadageschemas.validator(schema_name,schemadir).validate(data)
+    validopts = {
+        'schema_name': schema_name,
+        'schemadir': schemadir,
+    }
+    yadageschemas.validate_spec(data,validopts)
