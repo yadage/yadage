3cdf1323703bacec55165476a5ab0a7fc2555b33
lukasheinrich <lukas.heinrich@gmail.com>
2018-07-04 19:01:13 +0200

    demote log level

--- yadage/handlers/expression_handlers.py
+++ yadage/handlers/expression_handlers.py
@@ -100,9 +100,9 @@ def stage_output_selector(stageview, selection):
 @expression('fromvalue')
 def value_resolver(view, expression):
     value = view.getValue(expression['key'])
-    log.info('resolved to %s', value)
+    log.debug('resolved to %s', value)
     if isinstance(value,dict) and 'expression_type' in value:
-        log.info('looking up expression %s', value)
+        log.debug('looking up expression %s', value)
         return handlers[value['expression_type']](view, value)
     else:
         log.info('not an expression value')
