6f94293357e046609eed3a4709bf69346d0bf59c
lukasheinrich <lukas.heinrich@gmail.com>
2018-07-09 12:44:55 +0300

    rename

--- yadage/state_providers/__init__.py
+++ yadage/state_providers/__init__.py
@@ -7,13 +7,13 @@ log = logging.getLogger(__name__)
 
 from ..handlers.utils import handler_decorator
 
-providerhandlers, provider = handler_decorator()
+provider_deserialize_handlers, provider_deserializer = handler_decorator()
 
-@provider('localfs_provider')
+@provider_deserializer('localfs_provider')
 def localfs_provider(jsondata, deserialization_opts):
     return LocalFSProvider.fromJSON(jsondata, deserialization_opts)
 
-@provider('frompython_provider')
+@provider_deserializer('frompython_provider')
 def frompython_provider(jsondata, deserialization_opts):
     providerstring = deserialization_opts.get('state_provider','')
     _, module, providerclass = providerstring.split(':')
@@ -22,7 +22,7 @@ def frompython_provider(jsondata, deserialization_opts):
     provideropts = {}
     return providerclass.fromJSON(jsondata,**provideropts)
 
-@provider('fromenv_provider')
+@provider_deserializer('fromenv_provider')
 def fromenv_provider(jsondata, deserialization_opts):
     module = importlib.import_module(os.environ['YADAGE_STATEPROVIDER'])
     return module.load_provider(jsondata)
@@ -35,10 +35,10 @@ def load_provider(jsondata,deserialization_opts = None):
     if 'state_provider' in deserialization_opts:
         providerstring = deserialization_opts.get('state_provider','')
         if providerstring.startswith('py:'):
-            return providerhandlers['frompython_provider'](jsondata, deserialization_opts)
+            return provider_deserialize_handlers['frompython_provider'](jsondata, deserialization_opts)
     if 'YADAGE_STATEPROVIDER' in os.environ:
-        return providerhandlers['fromenv_provider'](jsondata, deserialization_opts)
+        return provider_deserialize_handlers['fromenv_provider'](jsondata, deserialization_opts)
 
-    return providerhandlers[jsondata['state_provider_type']](jsondata, deserialization_opts)
+    return provider_deserialize_handlers[jsondata['state_provider_type']](jsondata, deserialization_opts)
 
     raise TypeError('unknown provider type {}'.format(jsondata['state_provider_type']))
