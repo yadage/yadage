31e4cc04f7589403f985a3b49bc615ba87e4ef03
Lukas Heinrich <lukas.heinrich@gmail.com>
2019-09-27 11:05:12 +0200

    transparent_scope_selection

--- tests/test_utilcli.py
+++ tests/test_utilcli.py
@@ -3,34 +3,49 @@ import yadage.utilcli
 import yadage.steering
 import os
 
+
 def test_clis(tmpdir):
     runner = CliRunner()
 
-    result = runner.invoke(yadage.steering.main,[str(tmpdir.join('workdir')),'workflow.yml',
-        '-t','tests/testspecs/local-helloworld','-p','par=value',
-        '-b','foregroundasync'
-        ]
+    result = runner.invoke(
+        yadage.steering.main,
+        [
+            str(tmpdir.join("workdir")),
+            "workflow.yml",
+            "-t",
+            "tests/testspecs/local-helloworld",
+            "-p",
+            "par=value",
+            "-b",
+            "foregroundasync",
+        ],
     )
-    assert tmpdir.join('workdir/hello_world/hello_world.txt').check()
+    assert tmpdir.join("workdir/hello_world/hello_world.txt").check()
 
-
-    result = runner.invoke(yadage.utilcli.testsel,[
-        str(tmpdir.join('workdir/_yadage/yadage_snapshot_workflow.json')),
-        '{stages: hello_world, output: outputfile}'
-    ])
+    result = runner.invoke(
+        yadage.utilcli.testsel,
+        [
+            str(tmpdir.join("workdir/_yadage/yadage_snapshot_workflow.json")),
+            "{stages: hello_world, output: outputfile}",
+        ],
+    )
     assert result.exit_code == 0
 
+    result = runner.invoke(
+        yadage.utilcli.testsel,
+        [
+            str(tmpdir.join("workdir/_yadage/yadage_snapshot_workflow.json")),
+            "{stages: nonexistent, output: nonexistent}",
+        ],
+    )
+    assert result.exit_code != 0
 
-    result = runner.invoke(yadage.utilcli.testsel,[
-        str(tmpdir.join('workdir/_yadage/yadage_snapshot_workflow.json')),
-        '{stages: nonexistent, output: nonexistent}'
-    ])
-    assert result.exit_code == 0
-
-
-    result = runner.invoke(yadage.utilcli.viz,[
-        str(tmpdir.join('workdir/_yadage/yadage_snapshot_workflow.json')),
-        str(tmpdir.join('viz.pdf'))
-    ])
-    tmpdir.join('viz.pdf').check()
+    result = runner.invoke(
+        yadage.utilcli.viz,
+        [
+            str(tmpdir.join("workdir/_yadage/yadage_snapshot_workflow.json")),
+            str(tmpdir.join("viz.pdf")),
+        ],
+    )
+    tmpdir.join("viz.pdf").check()
     assert result.exit_code == 0
