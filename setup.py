982e2296d73eabdf4d047dbd6d11681a07aa437d
Matthew Feickert <matthew.feickert@cern.ch>
2022-02-03 01:22:40 -0600

    fix: Use adage 'viz' extra to provide dependencies for 'viz' extra (#108)
    * Use adage[viz] to provide pydot>=1.2.3 and pygraphviz as pip Issue
    3189 is resolved.
       - Set lower bound of adage[viz]>=0.10.3 to ensure proper lower
         bounds for pydot.
       - Results in pydot2 being dropped from viz extra.

--- setup.py
+++ setup.py
@@ -46,13 +46,7 @@ setup(
     install_requires=deps,
     extras_require={
         "celery": ["celery", "redis"],
-        "viz": [
-            # manually adding extras of adage[extra] because of pip
-            # issue https://github.com/pypa/pip/issues/3189
-            "pydot2",
-            "pygraphviz",
-            "pydotplus",
-        ],
+        "viz": ["adage[viz]>=0.10.3", "pydotplus>=2.0.0"],
         "lint": [
             "pyflakes",
             "isort",
